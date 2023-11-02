a281c982f658a241fdc4e5960e8334c2f91dcdd9
Marcos Coelho <mparav124@gmail.com>
2023-11-02 16:50:09 -0300

    add: home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,74 @@
 import React from 'react';
+import Header from '../header/Header';
+import { Link } from 'react-router-dom';
+import { ReactComponent as BallLogo } from '../../assets/icons/ball.svg';
+import { ReactComponent as TimeIcon } from '../../assets/icons/time.svg';
+import { ReactComponent as CalendarIcon } from '../../assets/icons/calendar.svg';
+import { ReactComponent as LocalIcon } from '../../assets/icons/local.svg';
+import { ReactComponent as ArrowIcon } from '../../assets/icons/arrow.svg';
+import { ReactComponent as Background } from '../../assets/icons/background.svg';
+import Avatar from 'boring-avatars';
+
+import data from '../../../data.json';
+
+import styles from './Home.module.css';
+
+const arrayColors = ['#CFE92F', '#E2F561', '#141414', '#DF8458'];
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Header>
+        <div className={styles.logo}>
+          <a href="/">
+            <BallLogo width={64} height={64} />
+          </a>
+        </div>
+        <div className={styles.avatar}>
+          <Avatar size={64} variant="bauhaus" colors={arrayColors} />
+        </div>
+      </Header>
+
+      <main className={styles.main}>
+        <h1 className={styles.titleSection}>Futs recentes</h1>
+
+        <ul className={styles.listCards}>
+          {data.map(({ title, hour, date, local, id }) => (
+            <li className={styles.card} key={id}>
+              <div className={styles.cardContent}>
+                <div className={styles.info}>
+                  <ul className={styles.details}>
+                    <li>
+                      <TimeIcon width={32} height={32} />
+                      {hour}
+                    </li>
+                    <li>
+                      <CalendarIcon width={32} height={32} />
+                      {date}
+                    </li>
+                    <li>
+                      <LocalIcon width={32} height={32} />
+                      {local}
+                    </li>
+                  </ul>
+
+                  <h2 className={styles.title}>{title}</h2>
+                </div>
+
+                <Link to="#" className={styles.button}>
+                  <ArrowIcon />
+                </Link>
+              </div>
+            </li>
+          ))}
+        </ul>
+
+        <Link className={styles.buttonAdd} to="/marcar-fut">
+          <BallLogo />
+        </Link>
+      </main>
+    </>
+  );
 };
 
 export default Home;
